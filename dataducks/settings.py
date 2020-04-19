feb3f607e4ab3afd9e750f3616e80d9da49490fd
ghabriel rodrigues <ghabriel.rodrigues.araujo@gmail.com>
2020-04-19 16:14:25 -0300

    [DEPLOY] creating static files folder

--- dataducks/settings.py
+++ dataducks/settings.py
@@ -14,8 +14,7 @@ import os
 from decouple import config, Csv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -69,6 +68,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'dataducks.urls'
@@ -148,7 +148,11 @@ if DEBUG:
 else:
     # Static files (CSS, JavaScript, Images)
     STATIC_URL = '/static/'
+    STATIC_TMP = os.path.join(BASE_DIR, 'static')
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    
+    os.makedirs(STATIC_TMP, exist_ok=True)
+    os.makedirs(STATIC_ROOT, exist_ok=True)
 
     # Extra places for collectstatic to find static files.
     STATICFILES_DIRS = (
